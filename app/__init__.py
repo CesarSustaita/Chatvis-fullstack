c2f9a203906da946a664273c3896e2ebe3eb416d
Oscar Sixtos <oscar.sixtos@me.com>
2023-11-24 05:14:13 -0600

    Add updated model

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import spacy
 app = Flask(__name__)
 
 # Load the spacy model
-app.nlp = spacy.load('spacy/model-best')
+app.nlp = spacy.load('spacy/model-best-x-eff')
 
 # Import the views after the Flask app is created
 from app import views
