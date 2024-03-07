29a0663b5cb87d6f16ce1ead517640159630461f
Cesar Sustaita <c.sustaita@outlook.com>
2024-03-07 09:14:30 -0600

    Add: Implement the example route and some comments

--- app/views.py
+++ app/views.py
@@ -13,6 +13,27 @@ def index():
     """
     return render_template('index.html')
 
+"""
+    This is a example to add new routes.
+    
+    -Remember it´s important that every route works,
+    -The route are: 
+        [USER VIEW]
+        -Index (must be added first navbar)
+        -Login (must be added first navbar)
+        -Register (must be added first navbar)
+        -Upload the file (must be added Second navbar)
+        -Conversation, chord diagrama and classification graph (it is currently) - (must be second navbar with its routes works correctly)
+
+        [ADMIN VIEW]
+        -Upload the file (must be added Second navbar and third navbar)
+        -Users registers(table) - (must be added Second navbar  and third navbar)
+        -Conversation, chord diagrama and classification graph (it is currently) -(must be second navbar with its routes works correctly and third navbar)
+"""
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/classify', methods=['POST'])
 def classify_message():
     """
