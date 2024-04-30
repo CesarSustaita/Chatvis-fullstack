9053b8dd227220f3b066606225906e7d931c0fe7
Oscar Sixtos <oscar.sixtos@me.com>
2024-04-30 13:39:30 -0600

    Move hardcoded keys to .keys untracked file

--- app/helpers.py
+++ app/helpers.py
@@ -5,7 +5,10 @@ from flask import session
 from werkzeug.security import check_password_hash
 import re
 import unicodedata
+from dotenv import load_dotenv
+import os
 
+load_dotenv('./.keys')
 
 def verify_recaptcha(recaptcha_response: str) -> bool:
     """
@@ -15,7 +18,7 @@ def verify_recaptcha(recaptcha_response: str) -> bool:
     Returns:
         bool: True if the reCAPTCHA response is valid, False otherwise.
     """
-    secret_key = "6Ld58aEpAAAAAMGcyyBsOv62fwY9LGWQCwS9xrl1"
+    secret_key = os.getenv("RECAPTCHA_SECRET_KEY")
     google_recaptcha_api_url = "https://www.google.com/recaptcha/api/siteverify"
     data = {"secret": secret_key, "response": recaptcha_response}
     try:
