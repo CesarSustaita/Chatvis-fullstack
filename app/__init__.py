e6de8901033b54751dbad8c0c8f4cf5dc4a920ca
Oscar Sixtos <oscar.sixtos@me.com>
2024-05-21 03:06:21 -0600

    Update run.py to include host and port parameters in app.run().
    Update __init__.py to configure app to run in dev or prod environments.
    
    Update navbar.html to improve responsiveness and styling of the navigation bar.
    
    Update register1.html to improve responsiveness and styling of the page.

--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,31 @@ import spacy
 from dotenv import load_dotenv
 import os
 
+# TODO: Cambiar a True en producción
+env_is_prod = False
+
 load_dotenv('./.keys')
 
-# TODO: Cambiar el prefijo SITE_PREFIX a "chatvis2024" en .keys
-prefix = os.getenv("SITE_PREFIX")
+# TODO: Cambiar el prefijo SITE_PREFIX a "chatvis2024" o según sea adecuado en .keys
+if env_is_prod:
+    prefix = os.getenv("SITE_PREFIX")
+else:
+    prefix = os.getenv("SITE_PREFIX_DEV")
 
-# Cambiar según si static está en la raíz o en la carpeta del prefijo
-#app = Flask(__name__, static_folder="static", static_url_path=f"/{prefix}/static")
-app = Flask(__name__, static_folder="static", static_url_path=f"/static")
+# TODO: Ajustar la ruta de la carpeta static según sea necesario
+if env_is_prod:
+    app = Flask(__name__, static_folder="static", static_url_path=f"/{prefix}/static")
+else:
+    app = Flask(__name__, static_folder="static", static_url_path="/static")
 
 app.prefix = prefix
 
-app.recaptcha_site_key = os.getenv("RECAPTCHA_SITE_KEY")
+if env_is_prod:
+    app.recaptcha_site_key = os.getenv("RECAPTCHA_SITE_KEY")
+else:
+    app.recaptcha_site_key = os.getenv("RECAPTCHA_SITE_KEY_DEV")
 
-# TODO: Set the secret key to a secure value
+# TODO: Set the secret key to a secure value in .keys
 app.config['SECRET_KEY'] = os.getenv("APP_SECRET_KEY")
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=15)
